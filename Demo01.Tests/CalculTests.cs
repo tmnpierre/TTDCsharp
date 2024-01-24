6c2fd7e982abe3a12a865aa02ab09c75e8cc5e64
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 14:23:05 +0100

    feat(MSTest): refactore CalculTest.cs

--- Demo01.Tests/CalculTests.cs
+++ Demo01.Tests/CalculTests.cs
@@ -5,14 +5,22 @@ namespace Demo01.Tests
     [TestClass]
     public class CalculTests //Tjrs en public
     {
+        private Calcul? _calcul;
+
+        // fonctionne mais pas recommandé pour NUnit et MSTest
+        //public CalculTests()
+        //{
+        //    _calcul = new();
+        //}
+
         [TestMethod]
         public void WhenAddition_10_30_Then_40()
         {
             // Arrange = On prepare les choses
-            var calcul = new Calcul();
+            // var calcul = new Calcul();
 
             // Act = On execute la fonction
-            var result = calcul.Addition(10, 30);
+            var result = _calcul.Addition(10, 30);
 
             // Assert = Verification dans le cadre des test
             Assert.AreEqual(40, result);
