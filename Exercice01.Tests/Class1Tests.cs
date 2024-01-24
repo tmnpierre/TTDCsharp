35979a74084a575f3279627d3aad9e801a2fe658
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 15:54:30 +0100

    feat(Exercice01): update project

--- Exercice01.Tests/Class1Tests.cs
+++ Exercice01.Tests/Class1Tests.cs
@@ -6,104 +6,24 @@ namespace Exercice01.Tests
     [TestFixture]
     public class Class1Tests
     {
-        [Test]
-        public void GetGrade_With95ScoreAnd90Attendance_ShouldReturnA()
+        [TestCase(95, 90, "A")]
+        [TestCase(85, 90, "B")]
+        [TestCase(65, 90, "C")]
+        [TestCase(95, 65, "B")]
+        [TestCase(95, 55, "F")]
+        [TestCase(65, 55, "F")]
+        [TestCase(50, 90, "F")]
+        public void WhenGetGrade(int n, int j, char grade)
         {
             var calculator = new GradingCalculator
             {
-                Score = 95,
-                AttendancePercentage = 90
+                Score = n,
+                AttendancePercentage = j
             };
 
             char result = calculator.GetGrade();
 
-            Assert.That(result, Is.EqualTo('A'));
-        }
-
-        [Test]
-        //- Score : 85%, Présence : 90 => Note: B
-        public void GetGrade_With85ScoreAnd90Attendance_ShouldReturnB()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 85,
-                AttendancePercentage = 90
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('B'));
-        }
-
-        [Test]
-        //- Score : 65%, Présence : 90 => Note: C
-        public void GetGrade_With65ScoreAnd90Attendance_ShouldReturnC()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 65,
-                AttendancePercentage = 90
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('C'));
-        }
-        [Test]
-        //- Score : 95%, Présence : 65 => Note: B
-        public void GetGrade_With95ScoreAnd65Attendance_ShouldReturnB()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 95,
-                AttendancePercentage = 65
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('B'));
-        }
-        [Test]
-        // - Score : 95%, Présence : 55 => Note: F
-        public void GetGrade_With95ScoreAnd55Attendance_ShouldReturnF()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 95,
-                AttendancePercentage = 55
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('F'));
-        }
-        [Test]
-        //- Score : 65%, Présence : 55 => Note: F
-        public void GetGrade_With65ScoreAnd55Attendance_ShouldReturnF()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 65,
-                AttendancePercentage = 55
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('F'));
-        }
-        [Test]
-        //- Score : 50%, Présence : 90 => Note: F
-        public void GetGrade_With50ScoreAnd90Attendance_ShouldReturnF()
-        {
-            var calculator = new GradingCalculator
-            {
-                Score = 50,
-                AttendancePercentage = 90
-            };
-
-            char result = calculator.GetGrade();
-
-            Assert.That(result, Is.EqualTo('F'));
+            Assert.That(result, Is.EqualTo(grade));
         }
     }
 }
