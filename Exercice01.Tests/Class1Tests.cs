caf28a6ef5fa1858ac95edf0b276e928c1bb9a7a
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 15:02:05 +0100

    feat(Exercice01): update project

--- Exercice01.Tests/Class1Tests.cs
+++ Exercice01.Tests/Class1Tests.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Exercice01.Bibliotheque;
+
 
 namespace Exercice01.Tests
 {
-    internal class Class1Tests
+    [TestFixture]
+    public class Class1Tests
     {
+        private GradingCalculator _gradingCalculator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _gradingCalculator = new GradingCalculator();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _gradingCalculator = null;
+        }
+
+        [Test]
+        //- Score : 95%, Présence : 90 => Note: A
+
+        [Test]
+        //- Score : 85%, Présence : 90 => Note: B
+
+        [Test]
+        //- Score : 65%, Présence : 90 => Note: C
+
+        [Test]
+        //- Score : 95%, Présence : 65 => Note: B
+
+        [Test]
+        // - Score : 95%, Présence : 55 => Note: F
+
+        [Test]
+        //- Score : 65%, Présence : 55 => Note: F
+
+        [Test]
+        //- Score : 50%, Présence : 90 => Note: F
     }
 }
