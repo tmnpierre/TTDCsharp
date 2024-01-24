39c2c1875a553bcaca559c324275c22a11b0fcbf
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 11:50:19 +0100

    feat(tests): add act and assert in CalculTests.cs

--- Demo01.Tests/CalculTests.cs
+++ Demo01.Tests/CalculTests.cs
@@ -9,6 +9,12 @@ namespace Demo01.Tests
         {
             // Arrange = On prepare les choses
             var calcul = new Calcul();
+
+            // Act = On execute la fonction
+            var result = calcul.Addition(10, 30);
+
+            // Assert = Verification dans le cadre des test
+            Assert.AreEqual(40, result);
         }
     }
 }
