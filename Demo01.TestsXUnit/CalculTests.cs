0e707d88676b87bc0e22ee5e1e8578e4be4eb5f3
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 14:09:34 +0100

    feat(refactor): xUnit synthax

--- Demo01.TestsXUnit/CalculTests.cs
+++ Demo01.TestsXUnit/CalculTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Demo01.Bibliotheque;
 
 namespace Demo01.TestsXUnit
 {
-    internal class CalculTests
+    public class CalculTests //Tjrs en public
     {
+        [Fact]
+        public void WhenAddition_10_30_Then_40()
+        {
+            // Arrange = On prepare les choses
+            var calcul = new Calcul();
+
+            // Act = On execute la fonction
+            var result = calcul.Addition(10, 30);
+
+            // Assert = Verification dans le cadre des test
+            //Assert.AreEqual(40, result);
+            Assert.Equal(40, result);
+        }
+
+        [Fact]
+        public void WhenDivision_30_10_Then_3()
+        {
+            var calcul = new Calcul();
+
+            var result = calcul.Division(30, 10);
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void WhenDivision_By_0_Throw_DivideByZeroException()
+        {
+            var calcul = new Calcul();
+
+            // Act & Assert
+            Assert.Throws<DivideByZeroException>(() => calcul.Division(10, 0));
+        }
     }
 }
