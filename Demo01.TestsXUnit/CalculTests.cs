655d2b41a50507775d3220d9e767476178f732d4
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 15:57:25 +0100

    feat(xUnit): refactore CalculTest.cs

--- Demo01.TestsXUnit/CalculTests.cs
+++ Demo01.TestsXUnit/CalculTests.cs
@@ -1,57 +1,58 @@
 ﻿using Demo01.Bibliotheque;
 
-namespace Demo01.TestsXUnit
+namespace Demo01.TestsXUnit;
+
+public class CalculTest : IDisposable
 {
-    public class CalculTests : IDisposable //Tjrs en public
+    private Calcul? _calcul;
+
+    // SetUp
+    public CalculTest()
     {
+        _calcul = new();
+    }
 
-        private Calcul? _calcul;
+    // TearDown
+    public void Dispose()
+    {
+        _calcul = null;
+    }
 
-        // SetUp
-        public CalculTests()
-        {
-            _calcul = new Calcul();
-        }
+    // IClassFixture à utiliser pour les OneTime...
 
-        // TearDown
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+    [Fact]
+    public void WhenAddition_10_30_Then_40()
+    {
+        // Arrange 
+        var calcul = new Calcul();
 
-        // IClassFixture à utiliser pour les OneTime...
+        // Act
+        var result = calcul.Addition(10, 30);
 
-        [Fact]
-        public void WhenAddition_10_30_Then_40()
-        {
-            // Arrange = On prepare les choses
-            var calcul = new Calcul();
+        // Assert
+        Assert.Equal(40, result);
+    }
 
-            // Act = On execute la fonction
-            var result = calcul.Addition(10, 30);
+    [Fact]
+    public void WhenDivision_30_10_Then_3()
+    {
+        // Arrange 
+        var calcul = new Calcul();
 
-            // Assert = Verification dans le cadre des test
-            //Assert.AreEqual(40, result);
-            Assert.Equal(40, result);
-        }
+        // Act
+        double result = calcul.Division(30, 10);
 
-        [Fact]
-        public void WhenDivision_30_10_Then_3()
-        {
-            var calcul = new Calcul();
+        // Assert
+        Assert.Equal(3, result);
+    }
 
-            var result = calcul.Division(30, 10);
+    [Fact]
+    public void WhenDivision_1_0_Then_DivideByZeroException()
+    {
+        // Arrange
+        var calcul = new Calcul();
 
-            Assert.Equal(3, result);
-        }
-
-        [Fact]
-        public void WhenDivision_By_0_Throw_DivideByZeroException()
-        {
-            var calcul = new Calcul();
-
-            // Act & Assert
-            Assert.Throws<DivideByZeroException>(() => calcul.Division(10, 0));
-        }
+        // Act et Assert
+        Assert.Throws<DivideByZeroException>(() => calcul.Division(1, 0));
     }
 }
