84d9dd350fb569c76ab83e25f988852af84c8722
tmnpierre <tomyn.pierre@gmail.com>
2024-01-24 11:28:17 +0100

    feat(tests): update CalculTests.cs

--- Demo01.Tests/CalculTests.cs
+++ Demo01.Tests/CalculTests.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Demo01.Bibliotheque;
 
 namespace Demo01.Tests
 {
     [TestClass]
     public class CalculTests //Tjrs en public
     {
-        public void WhenAddition_10_30_Then_40();
+        public void WhenAddition_10_30_Then_40()
         {
             // Arrange = On prepare les choses
             var calcul = new Calcul();
