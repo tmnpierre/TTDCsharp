b957f28ecc282c78ed81d08bc1ebb22024885f6c
tmnpierre <tomyn.pierre@gmail.com>
2024-01-25 09:58:16 +0100

    feat(tests): update LeapTesterTests.cs

--- Demo02TDD.Tests/LeapTesterTests.cs
+++ Demo02TDD.Tests/LeapTesterTests.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo02TDD.Core;
 
 namespace Demo02TDD.Tests
 {
+    [TestClass]
     public class LeapTesterTests
     {
+        [DataTestMethod]
+        [DataRow(1200)]
+        [DataRow(400)]
+        public void TestLeap_400_ShouldBe_True(int year)
+        {
+            LeapTester tester = new LeapTester();
+
+            bool result = tester.IsLeap(year); 
+
+            Assert.IsTrue(result);
+        }
     }
 }
