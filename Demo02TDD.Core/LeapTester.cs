4cee4bd7b81fc8b4553ded6974afc9ae444513f5
tmnpierre <tomyn.pierre@gmail.com>
2024-01-25 10:07:24 +0100

    feat(biblio): update LeapTester.cs

--- Demo02TDD.Core/LeapTester.cs
+++ Demo02TDD.Core/LeapTester.cs
@@ -12,6 +12,8 @@ namespace Demo02TDD.Core
         {
             if (year % 400 == 0) 
                 return true;
+            else if (year % 4 == 0 && year % 100 != 0)
+                return true;
             return false;
         }
     }
